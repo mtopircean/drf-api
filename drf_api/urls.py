a5330fbef11fc8e903f25894817108ece024cb7a
Marius Topircean <mtopircean@yahoo.com>
2024-01-05 19:47:48 +0000

    Added postlist permisions, urls and postserializer class

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),#adds the login logout functionality
     path('', include('profiles.urls')),
+    path('', include('posts.urls')),
 ]
